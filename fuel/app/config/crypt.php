702acf79da7036008ae5962c72db6cf3c92f8ef2
Primoz Rome <primozr@gmail.com>
2012-01-08 15:36:04 +0100

    ctypt config file added

---
+++ fuel/app/config/crypt.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package     Fuel
+ * @version     1.0
+ * @author      Fuel Development Team
+ * @license     MIT License
+ * @copyright   2011 Fuel Development Team
+ * @link        http://fuelphp.com
+ */
+
+return array (
+	'crypto_key' => '1SYExcx1ctsUiTQXuI8UY1o0',
+	'crypto_iv' => 'CncijMgUI8bI0DcHeolUwZTk',
+	'crypto_hmac' => 'PGokWoUHQA_MNssFGgesgEwg',
+);
+
+/* End of file config.php */
